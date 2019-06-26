c8614245375407f10e9f98af5bf1ba29e3fc93ec
ashappp <you@example.com>
2019-06-26 14:11:11 +0300

    add disabled button if on arr one el

--- client/src/components/MovieCard/MovieCard.jsx
+++ client/src/components/MovieCard/MovieCard.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { connect } from "react-redux";
-import getData from "../../redux/actions/getData";
-import s from "./MovieCard.module.css";
+import React from 'react';
+import { connect } from 'react-redux';
+import getData from '../../redux/actions/getData';
+import s from './MovieCard.module.css';
 
-const MovieCard = ({ item, deleteFilm }) => {
+const MovieCard = ({ filteredData, item, deleteFilm }) => {
   return (
     <li className={s.list_item} key={item._id}>
       <h2>{item.title}</h2>
@@ -17,18 +17,26 @@ const MovieCard = ({ item, deleteFilm }) => {
           </li>
         ))}
       </ul>
-      <button className={s.delete_btn} onClick={() => deleteFilm(item._id)}>
+      <button
+        className={s.delete_btn}
+        disabled={filteredData.length === 1 ? true : false}
+        onClick={() => deleteFilm(item._id)}
+      >
         delete
       </button>
     </li>
   );
 };
 
+const MSTP = store => ({
+  filteredData: store.filteredData,
+});
+
 const MDTP = dispatch => ({
-  deleteFilm: id => dispatch(getData.asyncDeleteDataAction(id))
+  deleteFilm: id => dispatch(getData.asyncDeleteDataAction(id)),
 });
 
 export default connect(
-  null,
-  MDTP
+  MSTP,
+  MDTP,
 )(MovieCard);
